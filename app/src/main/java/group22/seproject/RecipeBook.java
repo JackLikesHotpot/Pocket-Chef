d24fdd4606dc84831b6bc3750c2b8a6696f7c276
billy <billymoughton94@gmail.com>
2019-03-13 17:42:26 +0000

    Reorganised parts in pizza.txt so it's easier to extract Ingredient names. Reorganised parts of RecipeBook for the file input for each file.

--- app/src/main/java/group22/seproject/RecipeBook.java
+++ app/src/main/java/group22/seproject/RecipeBook.java
@@ -10,40 +10,116 @@ public class RecipeBook {
     private ArrayList<Recipe> recipeList = new ArrayList<Recipe>();
     private ArrayList<Ingredient> ingredientList = new ArrayList<Ingredient>();
 
-    private RecipeBook() throws IOException {
+    private RecipeBook() {
 
         // CODE TO LOOK THROUGH ALL FILES IN DATABASE (FOLDER) AND EXTRACT ALL RECIPES/INGREDIENTS AND INIT OBJECTSt
+        try {
 
-        BufferedReader inputStream;
-        String recipeName = "";
-        double recipeDuration = 0;
-        double recipeCalories = 0;
-        ArrayList<String> ingredients = new ArrayList<String>();
-        ArrayList<String> instructions = new ArrayList<String>();
+            String recipeName = "";
+            double recipeDuration = 0;
+            double recipeCalories = 0;
+            ArrayList<String> ingredients = new ArrayList<String>();
+            ArrayList<String> instructions = new ArrayList<String>();
 
-        File dir = new File("../../../../../sampledata/recipes"); // direcotry for all recipes
-        File[] dirListing = dir.listFiles();
-        if(dirListing != null) {
-            for(File recipeFile : dirListing) {
-
-                // COLLECT EACH FILE DATA, CREATE NEW OBJECTS, ADD TO RECIPEBOOK, ETC
-                inputStream = new BufferedReader(new FileReader(recipeFile)); // inputStream for current Recipe file
-                recipeName = inputStream.readLine();
-                recipeDuration = Double.parseDouble(inputStream.readLine());
-                recipeCalories = Double.parseDouble(inputStream.readLine());
+            String line;
+            int lineNumber;
+            int blankline;
+            FileReader fileReader;
+            BufferedReader bufferedReader;
 
 
+            // for every file in the recipes folder
+            File dir = new File("../../../../../sampledata/recipes"); // direcotry for all recipes
+            File[] dirListing = dir.listFiles();
+            if (dirListing != null) {
+                for (File recipeFile : dirListing) {
+
+                    // runs through current file and extracts data --> becomes new Recipe/Ingredient objects & added to RecipeBook
+                    line = null;
+                    lineNumber = 1;
+                    blankline = 0;
+
+                    fileReader = new FileReader(recipeFile);
+                    bufferedReader = new BufferedReader(fileReader);
+
+                    while ((line = bufferedReader.readLine()) != null) { // WILL THIS STOP AT LINE 4 WHEN THE LINE IS EMPTY??
+
+                        if (lineNumber == 1) {
+                            recipeName = line;
+                            lineNumber += 1;
+                        } else if (lineNumber == 2) {
+                            recipeCalories = Double.parseDouble(line);
+                            lineNumber += 1;
+                        } else if (lineNumber == 3) {
+                            recipeDuration = Double.parseDouble(line);
+                            lineNumber += 1;
+
+                        } else if (line.isEmpty()) {
+                            blankline += 1;
+
+                        } else {
+                            if (blankline == 1) {
+                                String ingredient = line;               //should ingredients be of objects or strings?
+                                ingredients.add(ingredient);            //same change for Recipe class?
+                            } else if (blankline == 2) {
+                                String instruction = line;
+                                instructions.add(instruction);
+                            }
+                        }
+                    }
+
+                    Recipe newR = new Recipe(recipeName, recipeCalories, recipeDuration, ingredients, instructions);
+                    this.recipeList.add(newR);
+
+                    bufferedReader.close();
+                }
 
 
 
 
-                inputStream.close();
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    /*inputStream = new BufferedReader(new FileReader(recipeFile)); // inputStream for current Recipe file
+                    recipeName = inputStream.readLine();
+                    recipeDuration = Double.parseDouble(inputStream.readLine());
+                    recipeCalories = Double.parseDouble(inputStream.readLine());
+                    inputStream.readLine();
+
+                    // LOOP FOR STORING INGREDIENTS IN ARRAY
+
+                    while(inputStream.readLine() != null) {
+                        ingredients.add(inputStream.readLine());
+                    }
+
+
+                    inputStream.close();*/
+
+
+            } else {
+                // HANDLE CASE WHERE IT IS NOT A DIRECTORY
             }
         }
+        catch(IOException e) {
 
-        else {
-            // HANDLE CASE WHERE IT IS NOT A DIRECTORY
         }
     }
 
