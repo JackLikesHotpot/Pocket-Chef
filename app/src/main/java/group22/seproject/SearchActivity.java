2fe3786d2655c991193a3bae89582ed7c09bb179
xjl98 <ijack.li.1998@gmail.com>
2019-03-18 17:57:24 +0000

    Added a front page that initialises the users.txt file. It redirects to the login page after a brief moment.

--- app/src/main/java/group22/seproject/SearchActivity.java
+++ app/src/main/java/group22/seproject/SearchActivity.java
@@ -1,13 +1,18 @@
 package group22.seproject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.File;
 
 public class SearchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
