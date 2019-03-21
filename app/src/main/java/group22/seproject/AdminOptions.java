f0d25e1dbce6c7d84bbab4d45f0fce763a8e01eb
billy <billymoughton94@gmail.com>
2019-03-21 17:34:15 +0000

    New files and edits for Admin Recipe Verification

--- app/src/main/java/group22/seproject/AdminOptions.java
+++ app/src/main/java/group22/seproject/AdminOptions.java
@@ -1,23 +1,57 @@
 package group22.seproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class AdminOptions extends Activity {
+
+    ListView verifyRecipes;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_screen);
 
-        Button verify = findViewById(R.id.verifyRecipe);
+        verifyRecipes = findViewById(R.id.verifyRecsLV);
+        final ArrayList<Recipe> items = RecipeBook.getInstance().getPendingRecipes();
+        ArrayList<String> recipeName = new ArrayList<String>();
 
-        verify.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        for (int i = 0; i < items.size(); i++) {
+            recipeName.add(items.get(i).getName());
+        }
 
-                //verify recipe
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(AdminOptions.this, android.R.layout.simple_list_item_1, recipeName);
+
+        verifyRecipes.setAdapter(arrayAdapter);
+
+        verifyRecipes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String selectedRecName = verifyRecipes.getItemAtPosition(position).toString();
+
+
+                for(int i = 0; i < items.size(); i++) {
+                    if(items.get(i).getName().equals(selectedRecName)) {
+                        Intent verifyRecPage = new Intent(AdminOptions.this, verifyRecipe.class);
+                        verifyRecPage.putExtra("RecipeName", items.get(i).getName());
+                        startActivity(verifyRecPage);
+                    }
+                }
+
             }
+
+
         });
+
+
     }
 }
---
+++ app/src/main/java/group22/seproject/verifyRecipe.java
@@ -0,0 +1,91 @@
+package group22.seproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class verifyRecipe extends AppCompatActivity {
+
+    Recipe pendingRecipe;
+    ArrayList<String> ingredientsString = new ArrayList<String>();
+    ArrayList<String> caloriesString = new ArrayList<String>();
+
+    TextView recipeNameTV;
+    ListView ingreidentsLV;
+    ListView caloriesLV;
+    TextView descriptionTV;
+    TextView durationTV;
+    TextView totalCalsTV;
+    Button acceptBtn;
+    Button rejectBtn;
+    final ArrayAdapter<String> adapter = new ArrayAdapter<String>(verifyRecipe.this, android.R.layout.simple_list_item_1, ingredientsString);
+    final ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(verifyRecipe.this, android.R.layout.simple_list_item_1, caloriesString);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.verify_recipe);
+
+        // fetch and create new Recipe object from pending list based on the passed recipe name
+
+        String recipeName = getIntent().getStringExtra("RecipeName");
+        ArrayList<Recipe> pendingList = RecipeBook.getInstance().getPendingRecipes();
+        for(int i = 0; i < pendingList.size(); i++) {
+            if(pendingList.get(i).getName().equals(recipeName)) {
+                pendingRecipe = pendingList.get(i);
+            }
+        }
+        ////////////////////////////////////////
+        ////////////////////////////////////////
+
+        // initialise all components from the verify_recipe.xml file
+        recipeNameTV = findViewById(R.id.recipeNameTV);
+        recipeNameTV.setText(pendingRecipe.getName());
+
+
+        ingreidentsLV = findViewById(R.id.ingredientsLV);
+        ingreidentsLV.setAdapter(adapter);
+        String currentIngName;
+        for(int i = 0; i < pendingRecipe.getIngredients().size(); i++) {
+            currentIngName = pendingRecipe.getIngredients().get(i).getName();
+            ingredientsString.add(currentIngName);
+        }
+
+        caloriesLV = findViewById(R.id.caloriesLV);
+        caloriesLV.setAdapter(adapter2);
+        double currentIngCal;
+        for(int i = 0; i < pendingRecipe.getIngredients().size(); i++) {
+            currentIngCal = pendingRecipe.getIngredients().get(i).getCalories();
+            caloriesString.add(Double.toString(currentIngCal));
+        }
+
+
+        descriptionTV = findViewById(R.id.descriptionTV);
+        for(int i = 0; i < pendingRecipe.getInstructions().size(); i++) {
+            descriptionTV.append(pendingRecipe.getInstructions().get(i));
+
+        }
+
+        durationTV = findViewById(R.id.durationTV);
+        durationTV.setText(Double.toString(pendingRecipe.getDuration()));
+
+        totalCalsTV = findViewById(R.id.totalCalsTV);
+        totalCalsTV.setText(Double.toString(pendingRecipe.getTotalcalories()));
+
+
+        acceptBtn = findViewById(R.id.acceptBtn);
+        rejectBtn = findViewById(R.id.rejectBtn);
+        ///////////////////////////////////////
+
+
+
+
+
+    }
+}
